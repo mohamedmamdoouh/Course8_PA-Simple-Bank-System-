41b1f93df1c3f159c0b8375bd5c78fbca4e845d4
Mohamed Mamdouh <ellempy888@gmail.com>
2024-12-13 16:56:19 +0200

    edited Course8.cpp

--- Course8.cpp
+++ Course8.cpp
@@ -1,10 +1,11 @@
 #include <bits/stdc++.h>
-#include "C:\Users\Mohamed Mamdouh\Documents\test_cpp\MyLib\myLib.h"
+#include "C:\Users\Mohamed Mamdouh\Documents\myGitHub\test_cpp\MyLib\myLib.h"
 using namespace std;
 
 int main()
 {
 
+    // this is a test to push to origin
 
     return 0;
 }
